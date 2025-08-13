ce4ed6c200e92ef9bbc3ead07512bb80d8e6ca53
ianshaloom <ianshaloom0@outlook.com>
2025-08-13 17:51:22 +0300

    feat: implement authentication flow with NextAuth and create login layout

---
+++ app/login/layout.tsx
@@ -0,0 +1,9 @@
+export const experimental_ppr = true;
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex justify-center content-center items-center h-screen">
+            <div>{children}</div>
+        </div>
+    );
+}
